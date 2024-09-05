f4c12ec1c049bb74701f21a8fb12828131498850
Robin <robinsingh@paisabazaar.com>
2024-09-05 18:14:21 +0530

    change done

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ var io = require('socket.io')(server)
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use(express.static(__dirname + "/build"))
+app.use("/",express.static(__dirname + "/build"))
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname + "/build/index.html"))
+	res.sendFile(path.resolve(__dirname, "build", "index.html"))
 })
 app.set('port', (process.env.PORT || 4001))
 
